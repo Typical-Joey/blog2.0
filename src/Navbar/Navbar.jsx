648b4026ebd486e2fb5431b653b738e4f0d728be
Typical-Joey <jtcapocci@yahoo.com>
2023-05-22 21:49:10 -0500

    App:  Added nav links

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -9,8 +9,9 @@ export default function MyNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
