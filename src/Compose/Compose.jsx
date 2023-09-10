04191679beac9e63c29a5987a6682be65ac1c1c2
Typical-Joey <jtcapocci@yahoo.com>
2023-09-10 03:10:18 -0500

    App:  Added form

--- src/Compose/Compose.jsx
+++ src/Compose/Compose.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Container, Form } from "react-bootstrap";
 import "./compose.css";
 
 export default function Compose() {
   return (
-    <div>
-      <h1>Compose Page</h1>
-    </div>
+    <Container className="compose-container">
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Title</Form.Label>
+          <Form.Control type="text" placeholder="Super Awesome Title" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Body</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="Write your post here..."
+          />
+        </Form.Group>
+      </Form>
+    </Container>
   );
 }
