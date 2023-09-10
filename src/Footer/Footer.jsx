1b6dd61b9ddda4de4320667a42babfccb9df0b73
Typical-Joey <jtcapocci@yahoo.com>
2023-09-10 13:48:52 -0500

    App:  Made footer sit on bottom of screen

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,9 +2,5 @@ import React from "react";
 import "./footer.css";
 
 export default function Footer() {
-  return (
-    <div className="footer-container">
-      <p>Typical Joey</p>
-    </div>
-  );
+  return <div className="footer-container"></div>;
 }
