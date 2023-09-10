7dc7744d5389ad1194988e6946c203eea185b1d1
Typical-Joey <jtcapocci@yahoo.com>
2023-09-10 03:21:31 -0500

    App:  Implemented styling for page

--- src/Compose/Compose.jsx
+++ src/Compose/Compose.jsx
@@ -6,18 +6,19 @@ export default function Compose() {
   return (
     <Container className="compose-container">
       <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Group className="mb-3" controlId="title">
           <Form.Label>Title</Form.Label>
           <Form.Control type="text" placeholder="Super Awesome Title" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Group className="mb-3" controlId="body">
           <Form.Label>Body</Form.Label>
           <Form.Control
             as="textarea"
-            rows={3}
+            rows={10}
             placeholder="Write your post here..."
           />
         </Form.Group>
+        <button className="submit-button">Post</button>
       </Form>
     </Container>
   );
