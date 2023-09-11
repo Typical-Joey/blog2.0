0f70c381b61773e96dc6df59fecaca63db174785
Typical-Joey <jtcapocci@yahoo.com>
2023-09-11 06:02:37 -0500

    App:  Updated fetch requests

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -2,7 +2,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 export async function getAllPosts() {
   try {
-    const posts = await fetch(API_URL);
+    const posts = await fetch(API_URL, { referrerPolicy: "unsafe_url" });
     return posts.json();
   } catch (error) {
     console.error(error);
@@ -12,7 +12,9 @@ export async function getAllPosts() {
 
 export async function getPostById(postId) {
   try {
-    const post = await fetch(`${API_URL}/${postId}`);
+    const post = await fetch(`${API_URL}/${postId}`, {
+      referrerPolicy: "unsafe_url",
+    });
     return post.json();
   } catch (error) {
     console.error(error);
@@ -25,6 +27,7 @@ export async function createPost(title, body) {
     const post = { postName: title, postBody: body };
     const response = await fetch(API_URL, {
       method: "POST",
+      referrerPolicy: "unsafe_url",
       headers: {
         "Content-Type": "application/json",
       },
